ec6235d7c6877b02c4f3f3947b79f6227b8c21b5
Jan Ignasik <ignasikj@gmail.com>
2019-11-14 22:15:04 +0100

    basic api

---
+++ Backend/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import database from "./database";
+import {getDataFromUrl} from "./etl";
+
+const main = async () => {
+  await database.init();
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.post("/extract", async function(req, res) {
+    const extractedData = await getDataFromUrl();
+    await database.insertExtracted(extractedData.map(url => ({url})));
+    res.send(200);
+  });
+
+  app.post("/transformed", async function(req, res) {
+  });
+
+  app.delete("/transformed", async function(req, res) {
+  });
+
+  app.delete("/extraced", async function(req, res) {
+  });
+
+  app.get("/transformed", async function(req, res) {
+  });
+
+  const server = app.listen(3000, function () {
+    console.log("app running on port.", server.address().port);
+  });
+};
+
+
+main()
